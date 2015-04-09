fdd19215ef4c1836ef92935ea2603b78205d92b2
Jing Wu <jingw@lnxsrv05.seas.ucla.edu>
2015-04-09 00:31:03 -0700

    mend

--- t.sh-18253.tmp/test.sh
+++
@@ -1,7 +0,0 @@
-true
-g++ -c foo.c
-: : :
-cat < /etc/passwd | tr a-z A-Z | sort -u || echo sort failed!
-a b<c > d
-cat < /etc/passwd | tr a-z A-Z | sort -u > out || echo sort failed!
-a<b>c|d<e>f|g<h>i
